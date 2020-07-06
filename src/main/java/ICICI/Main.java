4b9631afd207016fe282c0a58a0da272121e753a
Rohith <devops@capgemini.com>
2020-07-06 08:50:48 +0530

    developer coode for movie tickets

---
+++ src/main/java/ICICI/Main.java
@@ -0,0 +1,15 @@
+package ICICI;
+
+/**
+ * Class containing the "main" method
+ * prints Hello world! by default - replace with your code
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        //remove the line below and replace with your code.
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ICICI/MainTest.java
@@ -0,0 +1,22 @@
+package ICICI;
+
+import static junit.framework.TestCase.assertNotNull;
+import org.junit.Test;
+
+
+public class MainTest {
+
+
+    /**
+     * Test for the construction of Main and the 
+     * main method being called
+     *
+     */
+    @Test
+    public void shouldCreateMainObject(){
+        Main main = new Main();
+        assertNotNull("Main method called.", main);
+    }
+}
+
+
